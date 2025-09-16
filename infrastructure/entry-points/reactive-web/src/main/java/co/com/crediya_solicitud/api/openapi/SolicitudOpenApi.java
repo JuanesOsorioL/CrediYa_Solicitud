5291fe0576d7827273efd7bbe9b6dc11c690eac7
JuanesOsorioL <juanchis_1989@hotmail.com>
2025-09-16 18:43:48 -0500

    se finaliza hu 4

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya_solicitud/api/openapi/SolicitudOpenApi.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya_solicitud/api/openapi/SolicitudOpenApi.java
@@ -2,6 +2,7 @@ package co.com.crediya_solicitud.api.openapi;
 
 
 import co.com.crediya_solicitud.api.dto.SolicitudDto;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.experimental.UtilityClass;
 import org.springdoc.core.fn.builders.operation.Builder;
 import org.springframework.http.HttpStatus;
@@ -10,8 +11,10 @@ import org.springframework.web.ErrorResponse;
 
 import static org.springdoc.core.fn.builders.apiresponse.Builder.responseBuilder;
 import static org.springdoc.core.fn.builders.content.Builder.contentBuilder;
+import static org.springdoc.core.fn.builders.parameter.Builder.parameterBuilder;
 import static org.springdoc.core.fn.builders.requestbody.Builder.requestBodyBuilder;
 import static org.springdoc.core.fn.builders.schema.Builder.schemaBuilder;
+import static org.springdoc.core.fn.builders.securityrequirement.Builder.securityRequirementBuilder;
 
 @UtilityClass
 public class SolicitudOpenApi {
@@ -39,8 +42,35 @@ public class SolicitudOpenApi {
     public Builder findAll(Builder builder) {
         return builder
                 .operationId("findAll")
-                .description("Obtiene todos los usuarios registrados")
-                .tag("User")
+                .tag("Solicitud")
+                .description("Lista solicitudes por estado y paginación")
+
+                .parameter(parameterBuilder()
+                        .in(ParameterIn.QUERY)
+                        .name("status")
+                        .description("Estados separados por coma, ej: estado-003,estado-001")
+                        .required(false)
+                        .schema(schemaBuilder().implementation(String.class))
+                        .example("estado-003,estado-001"))
+
+                // page
+                .parameter(parameterBuilder()
+                        .in(ParameterIn.QUERY)
+                        .name("page")
+                        .description("Índice de página (>= 0)")
+                        .required(false)
+                        .schema(schemaBuilder().implementation(Integer.class))
+                        .example("0"))
+
+                // size
+                .parameter(parameterBuilder()
+                        .in(ParameterIn.QUERY)
+                        .name("size")
+                        .description("Tamaño de página (>= 1)")
+                        .required(false)
+                        .schema(schemaBuilder().implementation(Integer.class))
+                        .example("10"))
+
                 .response(responseBuilder().responseCode(String.valueOf(HttpStatus.OK.value()))
                         .description("Operación exitosa")
                         .content(contentBuilder().mediaType(MediaType.APPLICATION_JSON_VALUE)
@@ -48,6 +78,7 @@ public class SolicitudOpenApi {
                 .response(responseBuilder().responseCode(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description("Error interno")
                         .content(contentBuilder().mediaType(MediaType.APPLICATION_JSON_VALUE)
-                                .schema(schemaBuilder().implementation(ErrorResponse.class))));
+                                .schema(schemaBuilder().implementation(ErrorResponse.class))))
+                .security(securityRequirementBuilder().name("bearerAuth"));
     }
 }
